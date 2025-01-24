80cc90eea008d6fdba2c1731ec9ee2ec47b474c1
CodeGoy <144829693+CodeGoy@users.noreply.github.com>
2025-01-24 00:08:42 -0500

    example

--- .example/main.go
+++ .example/main.go
@@ -2,53 +2,74 @@ package main
 
 import (
 	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"github.com/CodeGoy/go-gqrx"
 	"log"
 )
 
+var (
+	version = "v0.0.2"
+)
+
+type Bookmarker struct {
+	app    fyne.App
+	client *gqrx.Client
+}
+
+type SdrData struct {
+	freq      int64
+	bandwidth int64
+	mode      string
+}
+
+func (b *Bookmarker) setSdrData(data *SdrData) error {
+	if err := b.client.SetDemod(data.mode, data.bandwidth); err != nil {
+		return fmt.Errorf("SetDemod error: %v", err)
+	}
+	if err := b.client.SetFreq(data.freq); err != nil {
+		return fmt.Errorf("SetFreq error: %v", err)
+	}
+	return nil
+}
+
+func (b *Bookmarker) getSdrData() (*SdrData, error) {
+	mode, bandwidth, err := b.client.GetDemod()
+	if err != nil {
+		return nil, fmt.Errorf("get mod err: %v", err)
+	}
+	freq, err := b.client.GetFreq()
+	if err != nil {
+		return nil, fmt.Errorf("get freq err: %v", err)
+	}
+	fmt.Println(mode, bandwidth, freq)
+	return &SdrData{freq: freq, bandwidth: bandwidth, mode: mode}, nil
+}
+
 func main() {
-	var station int64 = 93700000 // 93.7 FM
-	client := gqrx.NewClient("127.0.0.1:7356")
-	if err := client.Connect(); err != nil {
-		log.Fatalf("Error connecting: %v", err)
-		return
+	b := &Bookmarker{}
+	b.app = app.New()
+	addr := "127.0.0.1:7356"
+	b.client = gqrx.NewClient(addr)
+	if err := b.client.Connect(); err != nil {
+		log.Panicf("Error connecting: %v", err)
 	}
-	// get current mode and bandwidth
-	currentMode, currentBandwidth, err := client.GetDemod()
-	if err != nil {
-		log.Fatalf("Error getting current mode: %v", err)
-	}
-	// get current freq
-	currentFreq, err := client.GetFreq()
-	if err != nil {
-		log.Fatalf("Error getting frequency: %v", err)
-	}
-	fmt.Printf("Mode: %s\nBandwidth: %d\nFreq: %d\n", currentMode, currentBandwidth, currentFreq)
-	// mute audio
-	if err := client.SetMute(false); err != nil {
-		log.Fatalf("Error setting mute: %v", err)
-	}
-	// True on DPS if not running
-	dspStatus, err := client.GetDspStatus()
-	if err != nil {
-		log.Fatalf("Error getting DSP status: %v", err)
-	}
-	if !dspStatus {
-		err := client.SetDspStatus(true)
+	w := b.app.NewWindow(fmt.Sprintf("GQRX-Bookmarker v%s", version))
+	ng := container.NewGridWithColumns(6)
+	ng.Add(widget.NewButton("Add Bookmark", func() {
+		sdrData, err := b.getSdrData()
 		if err != nil {
-			log.Fatalf("Error setting DSP status: %v", err)
+			log.Printf("getSdrData error: %v", err)
+			return
 		}
-	}
-	// set mode and bandwidth, FM RADIO
-	if err := client.SetDemod("WFM_ST", 160000); err != nil {
-		log.Fatalf("Error setting demod: %v", err)
-	}
-	// set freq
-	if err := client.SetFreq(station); err != nil {
-		log.Fatalf("Error setting Freq: %v", err)
-	}
-	// disconnect
-	if err := client.Disconnect(); err != nil {
-		log.Fatalf("Error disconnecting: %v", err)
-	}
+		ng.Add(widget.NewButton(fmt.Sprintf("%.3f", float64(sdrData.freq)/1000000), func() {
+			if err := b.setSdrData(sdrData); err != nil {
+				log.Printf("setSdrData error: %v", err)
+			}
+		}))
+	}))
+	w.SetContent(ng)
+	w.ShowAndRun()
 }
